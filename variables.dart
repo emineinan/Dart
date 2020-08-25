b795818855624145703729566352ba09bd3010a3
emineinan <emineinan@users.noreply.github.com>
2020-08-25 19:09:57 +0300

    Example of using variables in dart language.

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main(){
+String message="Hello";
+int birthYear=1985;
+double weight=55.5;
+bool isEven=true;
+
+print(message);
+print("Her birth year is: "+birthYear.toString());
+print("Her weight is: "+weight.toString());
+print("Her lucky number is even?: "+isEven.toString());
+}
