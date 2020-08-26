d0cb048bb61ecfdf86c4d598cc16a6a6dc923780
emineinan <emineinan@users.noreply.github.com>
2020-08-26 11:18:00 +0300

    Example of using if else in dart language.

---
+++ ifElse.dart
@@ -0,0 +1,18 @@
+void main() {
+  var isSuccessful=true;
+
+  if(isSuccessful==true){
+  print("Student is successful");
+  }else{
+  print("Student is not successful");  
+  }
+
+  int score=60;
+  if(score>=80){
+  print("Perfect!");
+  }else if(score>=60){
+  print("Good");
+  }else if(score>=40){
+  print("Bad");
+  }
+}
