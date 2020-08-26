89f4087f34b6a2269d43dc5c3ad2a3202d81a78c
emineinan <emineinan@users.noreply.github.com>
2020-08-26 11:19:02 +0300

    Example of using switch-case in dart language.

---
+++ switchCase.dart
@@ -0,0 +1,16 @@
+void main() {
+  String letterGrade="BB";
+
+  switch(letterGrade){
+  case "AA": {print("Super");}
+  break;
+  case "BB": {print("Good");}
+  break;
+  case "CC": {print("Passable");}
+  break;
+  case "DD": {print("Bad");}
+  break;
+  default: {print("Letter grade unknown");}
+  break;
+  }
+}
