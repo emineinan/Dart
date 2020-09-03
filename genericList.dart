5f34556983bd135ddc9cb7f61b1f7d76ef0701a2
emineinan <56589369+emineinan@users.noreply.github.com>
2020-09-03 16:40:29 +0300

    Update genericList.dart

--- genericList.dart
+++ genericList.dart
@@ -1,12 +1,11 @@
 void main() {
-	Product product1=new Product("Laptop", 7000);
+  Product product1=new Product("Laptop", 7000);
   Product product2=new Product("Camera", 5000);
   List<Product> products=[product1, product2];
 
   print("Products");
   print(products[0].productName+" "+products[0].productPrice.toString());
   print(products[1].productName+" "+products[1].productPrice.toString());
-
 }
 
 class Product{
